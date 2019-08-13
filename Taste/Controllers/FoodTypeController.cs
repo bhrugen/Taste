dc9418de83fe5577fe3decb20a0a07419bc9f35c
Ben Spark <bhrugen1990@gmail.com>
2019-08-13 16:19:42 -0500

    Section 6 - Assignment 6 - Delete Food Type

--- Taste/Controllers/FoodTypeController.cs
+++ Taste/Controllers/FoodTypeController.cs
@@ -24,5 +24,18 @@ namespace Taste.Controllers
         {
             return Json(new { data = _unitOfWork.FoodType.GetAll() });
         }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.FoodType.GetFirstOrDefault(u => u.Id == id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting." });
+            }
+            _unitOfWork.FoodType.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete success." });
+        }
     }
 }
