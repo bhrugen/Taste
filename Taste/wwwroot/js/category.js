b7bf39977cc8b8ae1b1f048727d988c646fdaf92
Ben Spark <bhrugen1990@gmail.com>
2019-08-13 14:20:28 -0500

    Section 5 - Add Category Javascript

--- Taste/wwwroot/js/category.js
+++ Taste/wwwroot/js/category.js
@@ -1,1 +1,36 @@
-﻿
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadList();
+});
+
+function loadList() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/category",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "40%" },
+            { "data": "displayOrder", "width": "30%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return ` <div class="text-center">
+                                <a href="/Admin/category/upsert?id=${data}" class="btn btn-success text-white" style="cussor:pointer; width:100px;">
+                                    <i class="far fa-edit"></i> Edit
+                                </a>
+                                <a class="btn btn-danger text-white" style="cussor:pointer; width:100px;">
+                                    <i class="far fa-trash-alt"></i> Delete
+                                </a>
+                    </div>`;
+                }, "width": "30%"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found."
+        },
+        "width": "100%"
+    });
+}
