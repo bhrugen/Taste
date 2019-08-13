c164070388db5602a655940b03f44864a0e9cb6c
Ben Spark <bhrugen1990@gmail.com>
2019-08-13 08:13:54 -0500

    Section 3 - Class Libraries

---
+++ Taste.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Taste.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ Taste.Utility/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Taste.Utility
+{
+    public class Class1
+    {
+    }
+}
