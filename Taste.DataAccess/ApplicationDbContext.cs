17bafc1f43541bfef65b78a6c75f3e13ba51f3df
Ben Spark <bhrugen1990@gmail.com>
2019-08-13 11:48:02 -0500

    Section 4 - Implement CategoryRepository

--- Taste.DataAccess/ApplicationDbContext.cs
+++ Taste.DataAccess/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Taste.Models;
 
 namespace Taste.DataAccess
 {
@@ -12,5 +13,7 @@ namespace Taste.DataAccess
             : base(options)
         {
         }
+
+        public DbSet<Category> Category { get; set; }
     }
 }
--- Taste.DataAccess/Data/Repository/CategoryRepository.cs
+++ Taste.DataAccess/Data/Repository/CategoryRepository.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Taste.DataAccess.Data.Repository.IRepository;
+using Taste.Models;
 
 namespace Taste.DataAccess.Data.Repository
 {
-    class CategoryRepository
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<SelectListItem> GetCategoryListForDropDown()
+        {
+            return _db.Category.Select(i => new SelectListItem()
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            });
+        }
+
+        public void Update(Category category)
+        {
+            var objFromDb = _db.Category.FirstOrDefault(s => s.Id == category.Id);
+
+            objFromDb.Name = category.Name;
+            objFromDb.DisplayOrder = category.DisplayOrder;
+
+            _db.SaveChanges();
+
+        }
     }
 }
