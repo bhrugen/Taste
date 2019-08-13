284d6ac168f51366bc516cd9dbc250661b003e00
Ben Spark <bhrugen1990@gmail.com>
2019-08-13 14:28:45 -0500

    Section 5 - Category Get Page Handler

---
+++ Taste/Pages/Admin/Category/Upsert.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Taste.DataAccess.Data.Repository.IRepository;
+
+namespace Taste.Pages.Admin.Category
+{
+    public class UpsertModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpsertModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Models.Category CategoryObj { get; set; }
+
+        public IActionResult OnGet(int? id)
+        {
+            CategoryObj = new Models.Category();
+            if (id != null)
+            {
+                CategoryObj = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+                if (CategoryObj == null)
+                {
+                    return NotFound();
+                }
+            }
+            return Page();
+
+        }
+    }
+}
