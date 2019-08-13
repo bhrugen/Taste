b7d80d9721768c4c2acc69a9b129bd7b84549507
Ben Spark <bhrugen1990@gmail.com>
2019-08-13 15:41:08 -0500

    Section 5 - Delete Category

--- Taste/Controllers/CategoryController.cs
+++ Taste/Controllers/CategoryController.cs
@@ -24,5 +24,20 @@ namespace Taste.Controllers
         {
             return Json(new { data = _unitOfWork.Category.GetAll() });
         }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _unitOfWork.Category.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete successful" });
+        }
+
     }
 }
--- Taste/wwwroot/js/category.js
+++ Taste/wwwroot/js/category.js
@@ -21,7 +21,7 @@ function loadList() {
                                 <a href="/Admin/category/upsert?id=${data}" class="btn btn-success text-white" style="cussor:pointer; width:100px;">
                                     <i class="far fa-edit"></i> Edit
                                 </a>
-                                <a class="btn btn-danger text-white" style="cussor:pointer; width:100px;">
+                                <a class="btn btn-danger text-white" style="cursor:pointer; width:100px;" onclick=Delete('/api/category/'+${data})>
                                     <i class="far fa-trash-alt"></i> Delete
                                 </a>
                     </div>`;
@@ -32,5 +32,31 @@ function loadList() {
             "emptyTable": "no data found."
         },
         "width": "100%"
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure you want to Delete?",
+        text: "You will not be able to restore the data!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: 'DELETE',
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
     });
 }
