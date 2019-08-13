c9211663e5d9da075e3d0730a035ee2b7490dffb
Ben Spark <bhrugen1990@gmail.com>
2019-08-13 08:46:45 -0500

    Section 3 - Organizing Code

--- Taste.DataAccess/ApplicationDbContext.cs
+++ Taste.DataAccess/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Taste.Data
+namespace Taste.DataAccess
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- Taste.DataAccess/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Taste.DataAccess
-{
-    public class Class1
-    {
-    }
-}
