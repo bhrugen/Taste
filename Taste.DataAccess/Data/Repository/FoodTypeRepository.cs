31f794ef1ef4d3d7162615b1f0fd0e8c29655a8a
Ben Spark <bhrugen1990@gmail.com>
2019-08-13 15:49:39 -0500

    Section 6 - Assignment 2 - Add FoodType Repository and IRepository

---
+++ Taste.DataAccess/Data/Repository/FoodTypeRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Taste.DataAccess.Data.Repository.IRepository;
+using Taste.Models;
+
+namespace Taste.DataAccess.Data.Repository
+{
+    public class FoodTypeRepository : Repository<FoodType>, IFoodTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public FoodTypeRepository(ApplicationDbContext db)
+            : base(db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<SelectListItem> GetFoodTypeListForDropDown()
+        {
+            return _db.FoodType.Select(i => new SelectListItem()
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            });
+        }
+
+
+        public void Update(FoodType objectToBeUpdated)
+        {
+            var categoryFromDb = _db.FoodType.FirstOrDefault(s => s.Id == objectToBeUpdated.Id);
+            categoryFromDb.Name = objectToBeUpdated.Name;
+            _db.SaveChanges();
+        }
+
+    }
+}
---
+++ Taste.DataAccess/Data/Repository/IRepository/IFoodTypeRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Taste.Models;
+
+namespace Taste.DataAccess.Data.Repository.IRepository
+{
+    public interface IFoodTypeRepository : IRepository<FoodType>
+    {
+        IEnumerable<SelectListItem> GetFoodTypeListForDropDown();
+
+        void Update(FoodType foodType);
+    }
+}
