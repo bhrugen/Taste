202d14891b014da20f9f7dba6e2e7f13baf2d518
Ben Spark <bhrugen1990@gmail.com>
2019-08-13 14:47:26 -0500

    Section 5 - Create New Category

--- Taste/Pages/Admin/Category/Upsert.cshtml.cs
+++ Taste/Pages/Admin/Category/Upsert.cshtml.cs
@@ -17,6 +17,7 @@ namespace Taste.Pages.Admin.Category
             _unitOfWork = unitOfWork;
         }
 
+        [BindProperty]
         public Models.Category CategoryObj { get; set; }
 
         public IActionResult OnGet(int? id)
@@ -32,6 +33,25 @@ namespace Taste.Pages.Admin.Category
             }
             return Page();
 
+        }
+
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if(CategoryObj.Id == 0)
+            {
+                _unitOfWork.Category.Add(CategoryObj);
+            }
+            else
+            {
+                _unitOfWork.Category.Update(CategoryObj);
+            }
+            _unitOfWork.Save();
+            return RedirectToPage("./Index");
         }
     }
 }
