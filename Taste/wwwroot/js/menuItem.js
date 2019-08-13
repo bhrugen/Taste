2560d6d18809f26aadb7bd281b3aa8672bf3b70d
Ben Spark <bhrugen1990@gmail.com>
2019-08-13 18:39:45 -0500

    Section 7 - Menu Item Index Page

---
+++ Taste/wwwroot/js/menuItem.js
@@ -0,0 +1,64 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadList();
+});
+
+function loadList() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/MenuItem",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "25%" },
+            { "data": "price", "width": "15%" },
+            { "data": "category.name", "width": "15%" },
+            { "data": "foodType.name", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return ` <div class="text-center">
+                                <a href="/Admin/MenuItem/upsert?id=${data}" class="btn btn-success text-white" style="cussor:pointer; width:100px;">
+                                    <i class="far fa-edit"></i> Edit
+                                </a>
+                                <a class="btn btn-danger text-white" style="cursor:pointer; width:100px;" onclick=Delete('/api/MenuItem/'+${data})>
+                                    <i class="far fa-trash-alt"></i> Delete
+                                </a>
+                    </div>`;
+                }, "width": "30%"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found."
+        },
+        "width": "100%"
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure you want to Delete?",
+        text: "You will not be able to restore the data!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: 'DELETE',
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
