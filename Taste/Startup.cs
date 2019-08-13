b316321f2e2c835934104ca02313bcbc8138b23c
Ben Spark <bhrugen1990@gmail.com>
2019-08-13 08:21:33 -0500

    Section 3 - Manage Nuget Packages Part 1

--- Taste/Startup.cs
+++ Taste/Startup.cs
@@ -34,7 +34,7 @@ namespace Taste
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
