82059c044a55f4924b15594a690f3a46005c7eb9
Ben Spark <bhrugen1990@gmail.com>
2019-08-13 18:15:36 -0500

    Section 7 - MenuItem Controller Part 1

---
+++ Taste/Controllers/MenuItemController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Taste.DataAccess.Data.Repository.IRepository;
+
+namespace Taste.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public MenuItemController(IUnitOfWork unitOfWork, IWebHostEnvironment hostingEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Json(new { data = _unitOfWork.me.GetAll() });
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.FoodType.GetFirstOrDefault(u => u.Id == id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting." });
+            }
+            _unitOfWork.FoodType.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete success." });
+        }
+    }
+}
