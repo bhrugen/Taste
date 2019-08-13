f959748f6a20568418af3dbd5c3d32d85538f7a9
Ben Spark <bhrugen1990@gmail.com>
2019-08-13 16:14:07 -0500

    Section 6 - Assignment 4 - Display all Food Types

---
+++ Taste/Controllers/FoodTypeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Taste.DataAccess.Data.Repository.IRepository;
+
+namespace Taste.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodTypeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public FoodTypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Json(new { data = _unitOfWork.FoodType.GetAll() });
+        }
+    }
+}
---
+++ Taste/wwwroot/js/foodType.js
@@ -0,0 +1,43 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadList();
+});
+
+
+function loadList() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/FoodType",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            //should not be capital
+            { "data": "name", "width": "60%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center"> 
+                                <a href="/Admin/FoodType/upsert?id=${data}" class="btn btn-success text-white" style="cursor: pointer; width: 100px;" > 
+                                    <i class="far fa-edit"></i> Edit
+                                </a>
+                                <a class="btn btn-danger text-white" style="cursor: pointer; width: 100px;" onclick=Delete('/api/foodtype/'+${data})>  
+                                    <i class="far fa-trash-alt"></i> Delete
+                                </a>
+                            </div> `;
+
+                }, "width": "40%"
+            }
+
+
+        ],
+        "language": {
+            "emptyTable": "no data found."
+        },
+        "width": "100%"
+    });
+}
+
+
+
