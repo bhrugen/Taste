e0dddd6cc8e0484898649fc67b141b7e25ccf949
Ben Spark <bhrugen1990@gmail.com>
2019-08-13 16:18:01 -0500

    Section 6 - Assignment 5 - Food Type Upsert

---
+++ Taste/Pages/Admin/FoodType/Upsert.cshtml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Taste.DataAccess.Data.Repository.IRepository;
+
+namespace Taste.Pages.Admin.FoodType
+{
+    public class UpsertModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpsertModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [BindProperty]
+        public Models.FoodType FoodTypeObj { get; set; }
+
+        public IActionResult OnGet(int? id)
+        {
+            FoodTypeObj = new Models.FoodType();
+            if (id != null)
+            {
+                FoodTypeObj = _unitOfWork.FoodType.GetFirstOrDefault(u => u.Id == id);
+                if (FoodTypeObj == null)
+                {
+                    return NotFound();
+                }
+            }
+            return Page();
+
+        }
+
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if (FoodTypeObj.Id == 0)
+            {
+                _unitOfWork.FoodType.Add(FoodTypeObj);
+            }
+            else
+            {
+                _unitOfWork.FoodType.Update(FoodTypeObj);
+            }
+            _unitOfWork.Save();
+            return RedirectToPage("./Index");
+        }
+    }
+}
