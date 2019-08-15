5a3e0a44d553089df063a131ea40bea520dc06ab
Ben Spark <bhrugen1990@gmail.com>
2019-08-15 15:41:51 -0500

    Section 12 - Summary Get Handler

---
+++ Taste/Pages/Customer/Cart/Summary.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Taste.DataAccess.Data.Repository.IRepository;
+using Taste.Models;
+using Taste.Models.ViewModels;
+
+namespace Taste.Pages.Customer.Cart
+{
+    public class SummaryModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SummaryModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public OrderDetailsCart detailCart { get; set; }
+
+        public IActionResult OnGet()
+        {
+            detailCart = new OrderDetailsCart()
+            {
+                OrderHeader = new Models.OrderHeader()
+            };
+
+            detailCart.OrderHeader.OrderTotal = 0;
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            IEnumerable<ShoppingCart> cart = _unitOfWork.ShoppingCart.GetAll(c => c.ApplicationUserId == claim.Value);
+
+            if (cart != null)
+            {
+                detailCart.listCart = cart.ToList();
+            }
+
+            foreach (var cartList in detailCart.listCart)
+            {
+                cartList.MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault(m => m.Id == cartList.MenuItemId);
+                detailCart.OrderHeader.OrderTotal += (cartList.MenuItem.Price * cartList.Count);
+            }
+
+            ApplicationUser applicationUser = _unitOfWork.ApplicationUser.GetFirstOrDefault(c => c.Id == claim.Value);
+            detailCart.OrderHeader.PickupName = applicationUser.FullName;
+            detailCart.OrderHeader.PickUpTime = DateTime.Now;
+            detailCart.OrderHeader.PhoneNumber = applicationUser.PhoneNumber;
+            return Page();
+
+        }
+
+    }
+}
