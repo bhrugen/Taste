0696bc655eac88c48db76a28ea257b58d2820fab
Ben Spark <bhrugen1990@gmail.com>
2019-08-15 14:37:41 -0500

    Section 11 - Order Details Model

--- Taste.DataAccess/ApplicationDbContext.cs
+++ Taste.DataAccess/ApplicationDbContext.cs
@@ -19,5 +19,7 @@ namespace Taste.DataAccess
         public DbSet<MenuItem> MenuItem { get; set; }
         public DbSet<ApplicationUser> ApplicationUser { get; set; }
         public DbSet<ShoppingCart> ShoppingCart { get; set; }
+        public DbSet<OrderHeader> OrderHeader { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
     }
 }
--- Taste.Models/OrderDetails.cs
+++ Taste.Models/OrderDetails.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Taste.Models
 {
-    class OrderDetails
+    public class OrderDetails
     {
+        public int Id { get; set; }
+
+        [Required]
+        public int OrderId { get; set; }
+        [ForeignKey("OrderId")]
+        public virtual OrderHeader OrderHeader { get; set; }
+
+        [Required]
+        public int MenuItemId { get; set; }
+        [ForeignKey("MenuItemId")]
+        public virtual MenuItem MenuItem { get; set; }
+
+        public int Count { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
     }
 }
