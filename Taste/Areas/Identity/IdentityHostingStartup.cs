80aa0ddc6c5198766134b7ea33c170fea27bca74
Ben Spark <bhrugen1990@gmail.com>
2019-08-15 07:30:58 -0500

    Section 9 - Scaffold Identity

---
+++ Taste/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Taste.DataAccess;
+
+[assembly: HostingStartup(typeof(Taste.Areas.Identity.IdentityHostingStartup))]
+namespace Taste.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
