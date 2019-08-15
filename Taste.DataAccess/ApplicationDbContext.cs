027fc27c9596e703bd866c737df4cb19faeebebe
Ben Spark <bhrugen1990@gmail.com>
2019-08-15 11:03:27 -0500

    Section 10 - Add Shopping Cart Model

--- Taste.DataAccess/ApplicationDbContext.cs
+++ Taste.DataAccess/ApplicationDbContext.cs
@@ -18,5 +18,6 @@ namespace Taste.DataAccess
         public DbSet<FoodType> FoodType { get; set; }
         public DbSet<MenuItem> MenuItem { get; set; }
         public DbSet<ApplicationUser> ApplicationUser { get; set; }
+        public DbSet<ShoppingCart> ShoppingCart { get; set; }
     }
 }
---
+++ Taste.Models/ShoppingCart.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Taste.Models
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+            Count = 1;
+        }
+        public int Id { get; set; }
+
+        public int MenuItemId { get; set; }
+
+        [NotMapped]
+        [ForeignKey("MenuItemId")]
+        public virtual MenuItem MenuItem { get; set; }
+
+        public string ApplicationUserId { get; set; }
+
+        [NotMapped]
+        [ForeignKey("ApplicationUserId")]
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+
+        [Range(1,100, ErrorMessage ="Please select a count between 1 and 100")]
+        public int Count { get; set; }
+    }
+}
