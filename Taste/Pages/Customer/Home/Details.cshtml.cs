d4b28835a6d7b79fb0d4636e6a833bea44fa27e5
Ben Spark <bhrugen1990@gmail.com>
2019-08-15 12:04:04 -0500

    Section 10 - Add Items To Shopping Cart

--- Taste/Pages/Customer/Home/Details.cshtml.cs
+++ Taste/Pages/Customer/Home/Details.cshtml.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Taste.DataAccess.Data.Repository.IRepository;
 using Taste.Models;
+using Taste.Utility;
 
 namespace Taste.Pages.Customer.Home
 {
@@ -42,6 +44,28 @@ namespace Taste.Pages.Customer.Home
 
                 ShoppingCartObj.ApplicationUserId = claim.Value;
 
+                ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.GetFirstOrDefault(c => c.ApplicationUserId == ShoppingCartObj.ApplicationUserId &&
+                                            c.MenuItemId == ShoppingCartObj.MenuItemId);
+
+                if(cartFromDb == null)
+                {
+                    _unitOfWork.ShoppingCart.Add(ShoppingCartObj);
+                }
+                else
+                {
+                   _unitOfWork.ShoppingCart.IncrementCount(cartFromDb, ShoppingCartObj.Count);
+                }
+                _unitOfWork.Save();
+
+                var count = _unitOfWork.ShoppingCart.GetAll(c => c.ApplicationUserId == ShoppingCartObj.ApplicationUserId).ToList().Count;
+                HttpContext.Session.SetInt32(SD.ShoppingCart, count);
+                return RedirectToPage("Index");
+
+            }
+            else
+            {
+                ShoppingCartObj.MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault(includeProperties: "Category,FoodType", filter: c => c.Id == ShoppingCartObj.MenuItemId);
+                return Page();
             }
         }
     }
