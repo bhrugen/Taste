d53790ba0f7c9b52a589f4792262eea95efe9b2f
Ben Spark <bhrugen1990@gmail.com>
2019-08-15 11:13:41 -0500

    Section 10 - Details Get Handler

---
+++ Taste/Pages/Customer/Home/Details.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Taste.DataAccess.Data.Repository.IRepository;
+using Taste.Models;
+
+namespace Taste.Pages.Customer.Home
+{
+    [Authorize]
+    public class DetailsModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DetailsModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [BindProperty]
+        public ShoppingCart ShoppingCartObj { get; set; }
+
+        public void OnGet(int id)
+        {
+            ShoppingCartObj = new ShoppingCart()
+            {
+                MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault(includeProperties: "Category,FoodType", filter: c => c.Id == id),
+                MenuItemId = id
+            };
+        }
+    }
+}
