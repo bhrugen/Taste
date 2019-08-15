39481f170631f78b9ec9faacbc0c2c6665b62963
Ben Spark <bhrugen1990@gmail.com>
2019-08-15 12:19:21 -0500

    Section 10 - Load Shopping Cart Counter

--- Taste/Pages/Customer/Home/Index.cshtml.cs
+++ Taste/Pages/Customer/Home/Index.cshtml.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Taste.DataAccess.Data.Repository;
 using Taste.DataAccess.Data.Repository.IRepository;
 using Taste.Models;
+using Taste.Utility;
 
 namespace Taste.Pages.Customer.Home
 {
@@ -24,6 +27,16 @@ namespace Taste.Pages.Customer.Home
 
         public void OnGet()
         {
+
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claim != null)
+            {
+                int shoppingCartCount = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).ToList().Count;
+                HttpContext.Session.SetInt32(SD.ShoppingCart, shoppingCartCount);
+            }
+
             MenuItemList = _unitOfWork.MenuItem.GetAll(null, null, "Category,FoodType");
             CategoryList = _unitOfWork.Category.GetAll(null, q => q.OrderBy(c => c.DisplayOrder), null);
         }
