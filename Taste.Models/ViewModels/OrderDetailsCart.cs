59306ba1b8dff6aca134c90a9b2a79f912a768ec
Ben Spark <bhrugen1990@gmail.com>
2019-08-15 14:42:28 -0500

    Section 11 - Order Details Cart VM

---
+++ Taste.Models/ViewModels/OrderDetailsCart.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taste.Models.ViewModels
+{
+    public class OrderDetailsCart
+    { 
+        public List<ShoppingCart> listCart { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+    }
+}
