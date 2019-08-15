bc1c853e9b2c3ab23f3f1eef8e9d52e957a311ca
Ben Spark <bhrugen1990@gmail.com>
2019-08-15 14:55:25 -0500

    Section 11 - Shopping Cart Get Handler

--- Taste/Pages/Customer/Cart/Index.cshtml.cs
+++ Taste/Pages/Customer/Cart/Index.cshtml.cs
@@ -1,17 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Taste.DataAccess.Data.Repository.IRepository;
+using Taste.Models;
+using Taste.Models.ViewModels;
 
 namespace Taste.Pages.Customer.Cart
 {
     public class IndexModel : PageModel
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public IndexModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public OrderDetailsCart OrderDetailsCartVM { get; set; }
+
         public void OnGet()
         {
+            OrderDetailsCartVM = new OrderDetailsCart()
+            {
+                OrderHeader = new Models.OrderHeader()
+            };
 
+            OrderDetailsCartVM.OrderHeader.OrderTotal = 0;
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim =claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            IEnumerable<ShoppingCart> cart = _unitOfWork.ShoppingCart.GetAll(c => c.ApplicationUserId == claim.Value);
+
+            if(cart != null)
+            {
+                OrderDetailsCartVM.listCart = cart.ToList();
+            }
+
+            foreach(var cartList in OrderDetailsCartVM.listCart)
+            {
+                cartList.MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault(m => m.Id == cartList.MenuItemId);
+                OrderDetailsCartVM.OrderHeader.OrderTotal += (cartList.MenuItem.Price * cartList.Count);
+            }
         }
     }
 }
