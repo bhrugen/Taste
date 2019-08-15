9250a2357ad12f583378610ef2bc2ce5ed1413ac
Ben Spark <bhrugen1990@gmail.com>
2019-08-15 14:31:47 -0500

    Section 11 - Order Header Model

---
+++ Taste.Models/OrderHeader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Taste.Models
+{
+    public class OrderHeader
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [Display(Name ="Order Total")]
+        public double OrderTotal { get; set; }
+
+        [Required]
+        [Display(Name = "Pick Up Time")]
+        public DateTime PickUpTime { get; set; }
+
+        [Required]
+        [NotMapped]
+        public DateTime PickUpDate { get; set; }
+
+        public string Status { get; set; }
+
+        public string PaymentStatus { get; set; }
+        public string Comments { get; set; }
+
+        [Display(Name = "Pickup Name")]
+        public string PickupName { get; set; }
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+        public string TransactionId { get; set; }
+
+    }
+}
