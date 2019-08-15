89cc7d539fc235f725cd3f70a7f2435945cc6d20
Ben Spark <bhrugen1990@gmail.com>
2019-08-14 20:25:33 -0500

    Section 8 - Home Page Model

--- Taste/Pages/Customer/Home/Index.cshtml.cs
+++ Taste/Pages/Customer/Home/Index.cshtml.cs
@@ -4,14 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Taste.DataAccess.Data.Repository;
+using Taste.DataAccess.Data.Repository.IRepository;
+using Taste.Models;
 
 namespace Taste.Pages.Customer.Home
 {
     public class IndexModel : PageModel
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public IndexModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<MenuItem> MenuItemList { get; set; }
+        public IEnumerable<Category> CategoryList { get; set; }
+
         public void OnGet()
         {
-
+            MenuItemList = _unitOfWork.MenuItem.GetAll(null, null, "Category,FoodType");
+            CategoryList = _unitOfWork.Category.GetAll(null, q => q.OrderBy(c => c.DisplayOrder), null);
         }
     }
 }
