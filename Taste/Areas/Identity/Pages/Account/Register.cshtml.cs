228df6e7ad9d64da3571cadf2b451a938bbdb27c
Ben Spark <bhrugen1990@gmail.com>
2019-08-15 07:57:57 -0500

    Section 9 - Register Page UI Part 1

--- Taste/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Taste/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -63,6 +63,15 @@ namespace Taste.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            [Required]
+            public string FirstName { get; set; }
+            [Required]
+            public string LastName { get; set; }
+
+            [Required]
+            public string PhoneNumber { get; set; }
+
         }
 
         public async Task OnGetAsync(string returnUrl = null)
