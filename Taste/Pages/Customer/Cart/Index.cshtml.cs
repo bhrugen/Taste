90f30b3384f7f70f325de6f0ed899370dc556866
Ben Spark <bhrugen1990@gmail.com>
2019-08-15 15:26:51 -0500

    Section 11 - Shopping Cart Custom Handlers

--- Taste/Pages/Customer/Cart/Index.cshtml.cs
+++ Taste/Pages/Customer/Cart/Index.cshtml.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Taste.DataAccess.Data.Repository.IRepository;
 using Taste.Models;
 using Taste.Models.ViewModels;
+using Taste.Utility;
 
 namespace Taste.Pages.Customer.Cart
 {
@@ -46,6 +48,50 @@ namespace Taste.Pages.Customer.Cart
                 cartList.MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault(m => m.Id == cartList.MenuItemId);
                 OrderDetailsCartVM.OrderHeader.OrderTotal += (cartList.MenuItem.Price * cartList.Count);
             }
+        }
+
+        public IActionResult OnPostPlus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(c => c.Id == cartId);
+            _unitOfWork.ShoppingCart.IncrementCount(cart, 1);
+            _unitOfWork.Save();
+            return RedirectToPage("/Customer/Cart/Index");
+        }
+
+        public IActionResult OnPostMinus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(c => c.Id == cartId);
+            if (cart.Count == 1)
+            {
+                _unitOfWork.ShoppingCart.Remove(cart);
+                _unitOfWork.Save();
+
+                var cnt = _unitOfWork.ShoppingCart.
+                                GetAll(u => u.ApplicationUserId == cart.ApplicationUserId).ToList().Count;
+                HttpContext.Session.SetInt32(SD.ShoppingCart, cnt);
+            }
+            else
+            {
+                _unitOfWork.ShoppingCart.DecrementCount(cart, 1);
+                _unitOfWork.Save();
+
+            }
+
+           
+            return RedirectToPage("/Customer/Cart/Index");
+        }
+
+
+        public IActionResult OnPostRemove(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(c => c.Id == cartId);
+            _unitOfWork.ShoppingCart.Remove(cart);
+            _unitOfWork.Save();
+
+            var cnt = _unitOfWork.ShoppingCart.
+                               GetAll(u => u.ApplicationUserId == cart.ApplicationUserId).ToList().Count;
+            HttpContext.Session.SetInt32(SD.ShoppingCart, cnt);
+            return RedirectToPage("/Customer/Cart/Index");
         }
     }
 }
