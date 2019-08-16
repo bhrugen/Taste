3e20360a8444876edb57a493d9c602cc1c6c4393
Ben Spark <bhrugen1990@gmail.com>
2019-08-16 18:59:18 -0500

    Section 14 - Orders Controller API

---
+++ Taste.Models/ViewModels/OrderDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taste.Models.ViewModels
+{
+    public class OrderDetailsViewModel
+    {
+        public OrderHeader OrderHeader { get; set; }
+        public List<OrderDetails> OrderDetails { get; set; }
+    }
+}
--- Taste/Controllers/OrderController.cs
+++ Taste/Controllers/OrderController.cs
@@ -1,15 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Taste.DataAccess.Data.Repository.IRepository;
+using Taste.Models;
+using Taste.Models.ViewModels;
+using Taste.Utility;
 
 namespace Taste.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class OrderController : ControllerBase
+    public class OrderController : Controller
     {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<OrderDetailsViewModel> orderListVM = new List<OrderDetailsViewModel>();
+
+            IEnumerable<OrderHeader> OrderHeaderList;
+
+            if (User.IsInRole(SD.CustomerRole))
+            {
+                //retrieve all orders for that customer
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                OrderHeaderList = _unitOfWork.OrderHeader.GetAll(u => u.UserId == claim.Value, null, "ApplicationUser");
+            }
+            else
+            {
+                OrderHeaderList = _unitOfWork.OrderHeader.GetAll(null, null, "ApplicationUser");
+            }
+
+
+            foreach(OrderHeader item in OrderHeaderList)
+            {
+                OrderDetailsViewModel individual = new OrderDetailsViewModel
+                {
+                    OrderHeader = item,
+                    OrderDetails = _unitOfWork.OrderDetails.GetAll(o => o.OrderId == item.Id).ToList()
+                };
+                orderListVM.Add(individual);
+            }
+
+            return Json(new { data = orderListVM });
+        }
+
     }
 }
