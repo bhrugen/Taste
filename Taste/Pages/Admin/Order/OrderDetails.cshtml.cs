9c44d48e04a557bf4c39f5236bb1d67f2b2cc105
Ben Spark <bhrugen1990@gmail.com>
2019-08-16 19:51:55 -0500

    Section 14 - Order Details Get Handler

---
+++ Taste/Pages/Admin/Order/OrderDetails.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Taste.DataAccess.Data.Repository.IRepository;
+using Taste.Models.ViewModels;
+
+namespace Taste.Pages.Admin.Order
+{
+    public class OrderDetailsModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderDetailsModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [BindProperty]
+        public OrderDetailsViewModel OrderDetailsVM { get; set; }
+
+
+        public void OnGet(int id)
+        {
+            OrderDetailsVM = new OrderDetailsViewModel()
+            {
+                OrderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(m => m.Id == id),
+                OrderDetails = _unitOfWork.OrderDetails.GetAll(m => m.OrderId == id).ToList()
+
+            };
+
+            OrderDetailsVM.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser.GetFirstOrDefault(u => u.Id == OrderDetailsVM.OrderHeader.UserId);
+        }
+    }
+}
