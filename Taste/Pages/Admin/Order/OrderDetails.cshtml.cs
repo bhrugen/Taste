d3ea944fe0a56f5f710b96bf4761cda39010ddc7
Ben Spark <bhrugen1990@gmail.com>
2019-08-17 11:28:16 -0500

    Section 14 - Order Management on Order Details

--- Taste/Pages/Admin/Order/OrderDetails.cshtml.cs
+++ Taste/Pages/Admin/Order/OrderDetails.cshtml.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Stripe;
 using Taste.DataAccess.Data.Repository.IRepository;
+using Taste.Models;
 using Taste.Models.ViewModels;
+using Taste.Utility;
 
 namespace Taste.Pages.Admin.Order
 {
@@ -32,6 +35,42 @@ namespace Taste.Pages.Admin.Order
             };
 
             OrderDetailsVM.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser.GetFirstOrDefault(u => u.Id == OrderDetailsVM.OrderHeader.UserId);
+        }
+
+
+
+        public IActionResult OnPostOrderConfirm(int orderId)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(o => o.Id == orderId);
+            orderHeader.Status = SD.StatusCompleted;
+            _unitOfWork.Save();
+            return RedirectToPage("OrderList");
+        }
+
+        public IActionResult OnPostOrderCancel(int orderId)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(o => o.Id == orderId);
+            orderHeader.Status = SD.StatusCancelled;
+            _unitOfWork.Save();
+            return RedirectToPage("OrderList");
+        }
+
+        public IActionResult OnPostOrderRefund(int orderId)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(o => o.Id == orderId);
+            //refund amount
+            var options = new RefundCreateOptions
+            {
+                Amount = Convert.ToInt32(orderHeader.OrderTotal * 100),
+                Reason = RefundReasons.RequestedByCustomer,
+                ChargeId = orderHeader.TransactionId
+            };
+            var service = new RefundService();
+            Refund refund = service.Create(options);
+
+            orderHeader.Status = SD.StatusRefunded;
+            _unitOfWork.Save();
+            return RedirectToPage("OrderList");
         }
     }
 }
