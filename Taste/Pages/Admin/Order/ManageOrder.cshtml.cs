e951518ca1b49d733d4f44862a714b5a05b70a0d
Ben Spark <bhrugen1990@gmail.com>
2019-08-16 20:39:37 -0500

    Section 14 - Manage Order Get Handler

---
+++ Taste/Pages/Admin/Order/ManageOrder.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Taste.DataAccess.Data.Repository.IRepository;
+using Taste.Models;
+using Taste.Models.ViewModels;
+using Taste.Utility;
+
+namespace Taste.Pages.Admin.Order
+{
+    public class ManageOrderModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ManageOrderModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [BindProperty]
+        public List<OrderDetailsViewModel> orderDetailsVM { get; set; }
+
+        
+        public void OnGet()
+        {
+            orderDetailsVM = new List<OrderDetailsViewModel>();
+
+            List<OrderHeader> OrderHeaderList = _unitOfWork.OrderHeader
+                .GetAll(o => o.Status == SD.StatusSubmitted || o.Status == SD.StatusInProcess)
+                .OrderByDescending(u => u.PickUpTime).ToList();
+
+
+                   foreach (OrderHeader item in OrderHeaderList)
+            {
+                OrderDetailsViewModel individual = new OrderDetailsViewModel
+                {
+                    OrderHeader = item,
+                    OrderDetails = _unitOfWork.OrderDetails.GetAll(o => o.OrderId == item.Id).ToList()
+                };
+                orderDetailsVM.Add(individual);
+            }
+        }
+
+    }
+}
