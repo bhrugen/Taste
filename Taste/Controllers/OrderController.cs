4e4433ea4b4cb97bc563d3917a8930a9d77dd965
Ben Spark <bhrugen1990@gmail.com>
2019-09-01 11:31:27 -0500

    Section 14 - Order History

--- Taste/Controllers/OrderController.cs
+++ Taste/Controllers/OrderController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Taste.DataAccess.Data.Repository.IRepository;
@@ -25,6 +26,7 @@ namespace Taste.Controllers
         }
 
         [HttpGet]
+        [Authorize]
         public IActionResult Get(string status = null)
         {
             List<OrderDetailsViewModel> orderListVM = new List<OrderDetailsViewModel>();
