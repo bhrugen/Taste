6528c03736f210a83f0e3baa6e9aee1ff0796c2c
Ben Spark <bhrugen1990@gmail.com>
2019-11-03 16:27:09 -0600

    Section 16 - SP_Call Part 2

--- Taste.DataAccess/Data/Repository/SP_Call.cs
+++ Taste.DataAccess/Data/Repository/SP_Call.cs
@@ -2,30 +2,53 @@
 using System.Collections.Generic;
 using System.Text;
 using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Taste.DataAccess.Data.Repository.IRepository;
 
 namespace Taste.DataAccess.Data.Repository
 {
     class SP_Call : ISP_Call
     {
+        private readonly ApplicationDbContext _db;
+        private static string ConnectionString = "";
+
+        public SP_Call(ApplicationDbContext db)
+        {
+            _db = db;
+            ConnectionString = db.Database.GetDbConnection().ConnectionString;
+        }
+
         public void Dispose()
         {
-            throw new NotImplementedException();
+            _db.Dispose();
         }
 
         public T ExecureReturnScaler<T>(string procedureName, DynamicParameters param = null)
         {
-            throw new NotImplementedException();
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                sqlCon.Open();
+                return (T)Convert.ChangeType(sqlCon.ExecuteScalar<T>(procedureName, param, commandType: System.Data.CommandType.StoredProcedure), typeof(T));
+            }
         }
 
         public void ExecuteWithoutReturn(string procedureName, DynamicParameters param = null)
         {
-            throw new NotImplementedException();
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                sqlCon.Open();
+                sqlCon.Execute(procedureName, param, commandType: System.Data.CommandType.StoredProcedure);
+            }
         }
 
         public IEnumerable<T> ReturnList<T>(string procedureName, DynamicParameters param = null)
         {
-            throw new NotImplementedException();
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                sqlCon.Open();
+                return sqlCon.Query<T>(procedureName, param, commandType: System.Data.CommandType.StoredProcedure);
+            }
         }
     }
 }
